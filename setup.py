8339e4f114186adeaec116d61c2ee49371678aa5
iamshubhp <shubhpatel1812002@gmail.com>
2025-03-13 04:22:47 -0400

    Requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='wasteDetection',
+    version='0.0.0',
+    author='shubhpatel',
+    author_email='shubhp1801@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
